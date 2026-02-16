e67d366d2676b5588617eceec4c0ecf5e941378f
Huang, Zeyu <11222265+fhfuih@users.noreply.github.com>
2026-02-16 15:37:27 +0800

    bash

---
+++ dot_config/blesh/init.sh
@@ -0,0 +1,8 @@
+ble/path#remove-glob PATH '/mnt/*'
+
+ble-face auto_complete='fg=239'      # fish-style completion candidate: gray
+ble-face menu_filter_input='none'    # type after showing Tab completion candidate: no extra coloring
+ble-face filename_directory='fg=096' # Folder path: purple
+ble-face filename_other='none'       # File path: plain white
+
+ble-face command_builtin='fg=green' # Built-in command: green not red
