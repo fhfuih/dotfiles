14b25086ec9740629114bded67e878c1e7a409a1
Huang, Zeyu <11222265+fhfuih@users.noreply.github.com>
2026-04-13 23:27:21 +0800

    nvim

--- dot_config/nvim/lua/plugins/formatting.lua
+++ dot_config/nvim/lua/plugins/formatting.lua
@@ -4,8 +4,13 @@ return {
     opts = function(_, opts)
       opts.formatters_by_ft = opts.formatters_by_ft or {}
       opts.formatters_by_ft.tex = { "tex-fmt" }
+      opts.formatters_by_ft.json = { "oxfmt" }
 
       opts.formatters = opts.formatters or {}
+      -- opts.formatters.oxlint = {
+      --   command = "oxlint",
+      --   args = { "--fix", "$FILENAME" },
+      -- }
     end,
   },
 }
